645266d47f91c2a7b0d4cfca6b88307c9ad4fc51
benbenlaw <benlawrenceben@hotmail.co.uk>
2024-10-29 16:00:14 +0000

    2.8.2

--- src/main/java/com/benbenlaw/strainers/datagen/StrainersLootTableProvider.java
+++ src/main/java/com/benbenlaw/strainers/datagen/StrainersLootTableProvider.java
@@ -3,10 +3,16 @@ package com.benbenlaw.strainers.datagen;
 import com.benbenlaw.strainers.block.ModBlocks;
 import it.unimi.dsi.fastutil.objects.ReferenceOpenHashSet;
 import net.minecraft.core.HolderLookup;
+import net.minecraft.core.component.DataComponents;
 import net.minecraft.data.loot.packs.VanillaBlockLoot;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.storage.loot.LootPool;
 import net.minecraft.world.level.storage.loot.LootTable;
+import net.minecraft.world.level.storage.loot.entries.LootItem;
+import net.minecraft.world.level.storage.loot.functions.CopyComponentsFunction;
+import net.minecraft.world.level.storage.loot.providers.number.ConstantValue;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Set;
@@ -19,10 +25,14 @@ public class StrainersLootTableProvider extends VanillaBlockLoot {
     @Override
     protected void generate() {
 
-        this.dropSelf(ModBlocks.STRAINER_TANK.get());
         this.dropSelf(ModBlocks.WOODEN_STRAINER.get());
         this.dropSelf(ModBlocks.ORE_MULCH.get());
         this.dropSelf(ModBlocks.MULCH.get());
+
+        this.add(ModBlocks.STRAINER_TANK.get(),loot -> LootTable.lootTable()
+                .withPool(new LootPool.Builder().add(LootItem.lootTableItem(ModBlocks.STRAINER_TANK)
+                .apply(CopyComponentsFunction.copyComponents(CopyComponentsFunction.Source.BLOCK_ENTITY))
+                )));
 
     }
 
